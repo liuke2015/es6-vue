388d87dd7f5007380694b29e62ddf7b203584c68
liuke2015 <376060613@qq.com>
2019-09-27 17:12:40 +0800

    新增模块化引入(import)的4种方法

--- src/components/testES6/testES6.vue
+++ src/components/testES6/testES6.vue
@@ -5,6 +5,11 @@
 <script>
   import "@assets/js/lesson17"
 
+  /*import {A,test,Hello} from "@assets/js/lesson18"*/
+  /*import {A} from "@assets/js/lesson18"*/
+  /*import * as lesson from "@assets/js/lesson18"*/
+  /*import lesson18 from "@assets/js/lesson18"*///exprot default的时候用这种方法
+
   export default {
     name: "testES6",
     msg: "es6js练习入口文件",
