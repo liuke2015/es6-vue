39f5eb7100bc10b94da5f8c9757aff8417e9f12a
liuke2015 <376060613@qq.com>
2019-09-27 18:01:53 +0800

    新增模块:基本模块、计算模块、接口模块、倒计时模块；倒计时模块已完成内容

---
+++ src/assets/lottery.js
@@ -0,0 +1,5 @@
+/*整合lottery文件夹中js文件的模块*/
+import "./lottery/base"
+import "./lottery/timer"
+import "./lottery/calculate"
+import "./lottery/interface"
--- src/components/testES6/testES6.vue
+++ src/components/testES6/testES6.vue
@@ -3,12 +3,17 @@
 </template>
 
 <script>
-  import "@assets/js/lesson17"
+  //以下是语法练习的引入文件
+  /*import "@assets/js/lesson17"*/
 
   /*import {A,test,Hello} from "@assets/js/lesson18"*/
   /*import {A} from "@assets/js/lesson18"*/
   /*import * as lesson from "@assets/js/lesson18"*/
   /*import lesson18 from "@assets/js/lesson18"*///exprot default的时候用这种方法
+
+  //以下是项目实战的引入文件
+  /*import "babel-polyfill";*/
+  import lottery from "@assets/lottery"
 
   export default {
     name: "testES6",
